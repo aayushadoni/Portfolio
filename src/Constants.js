75a591f6e90f625d044bb552b05ecfe274d68465
aayushadoni <78133084+aayushadoni@users.noreply.github.com>
2023-06-14 15:42:03 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -3,7 +3,7 @@ export const GITHUB_URL = "https://github.com/aayushadoni";
 export const RESUME_URL = "https://drive.google.com/file/d/1Zs936n-y7DiZV5J3Td2kxHXYQ5UCf9rx/view?usp=sharing";
 export const INSTAGRAM_URL = "https://www.instagram.com/snapsbyaayush/";
 export const EMAIL_URL = "mailto:aayushadoni1@gmail.com";
-export const ARTSTATIONURL = "https://www.artstation.com/kawal2001singh"
+export const ARTSTATIONURL = ""
 export const LOCATION_URL = "https://goo.gl/maps/p4zDh3Kg4Uhunes6A";
 export const PROFILE_PHOTO_URL = "https://img.freepik.com/free-vector/hacker-operating-laptop-cartoon-icon-illustration-technology-icon-concept-isolated-flat-cartoon-style_138676-2387.jpg?w=360";
 
