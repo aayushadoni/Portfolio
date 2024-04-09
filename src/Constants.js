a91a7dc8f7a992cf5f5780c1a033d7831f4524cd
aayushadoni <78133084+aayushadoni@users.noreply.github.com>
2024-04-09 19:20:56 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 export const LINKEDIN_URL = "https://www.linkedin.com/in/aayush-adoni-1b3944231/";
 export const GITHUB_URL = "https://github.com/aayushadoni";
-export const RESUME_URL = "https://drive.google.com/file/d/1Zs936n-y7DiZV5J3Td2kxHXYQ5UCf9rx/view?usp=sharing";
+export const RESUME_URL = "https://drive.google.com/file/d/1-AsSwb-v2WE0P9M8vYAq40q8X1GCNO2p/view?usp=sharing";
 export const INSTAGRAM_URL = "https://www.instagram.com/snapsbyaayush/";
 export const EMAIL_URL = "mailto:aayushadoni1@gmail.com";
 export const ARTSTATIONURL = ""
